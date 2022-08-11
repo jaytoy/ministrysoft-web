2b54e0936de3ad1476db590428ef0bd4747e58f3
Jay Toy <jay_toyjianying@hotmail.com>
2022-08-11 14:35:37 +0200

    add table component

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import Image from 'next/image'
+import Table from '../components/table'
 
 const user = {
   name: 'Tom Cook',
@@ -187,9 +188,7 @@ export default function Dashboard() {
           <main>
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
               {/* Replace with your content */}
-              <div className="px-4 py-8 sm:px-0">
-                <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-              </div>
+              <Table />
               {/* /End replace */}
             </div>
           </main>
